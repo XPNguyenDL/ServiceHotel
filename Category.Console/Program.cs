ab0d2404e2dc42fb89b8447cfc6e64b6edb3547a
2011416_nhan <2011416@dlu.edu.vn>
2023-05-15 19:41:30 +0700

    comment code

--- Category.Console/Program.cs
+++ Category.Console/Program.cs
@@ -12,25 +12,25 @@ seeder.Initialize();
 
 // Change category delete status
 #region 
-Console.WriteLine("".PadRight(80, '-'));
-Console.WriteLine("Change Category delete status");
-var categoryDelete = await categoryRepository.ChangeCategoryDeleteStatusAsync(2);
-if (categoryDelete)
-    Console.WriteLine("Change successfully.");
-else
-    Console.WriteLine("Change failed.");
-Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("Change Category delete status");
+//var categoryDelete = await categoryRepository.ChangeCategoryDeleteStatusAsync(2);
+//if (categoryDelete)
+//    Console.WriteLine("Change successfully.");
+//else
+//    Console.WriteLine("Change failed.");
+//Console.WriteLine("".PadRight(80, '-'));
 #endregion
 
 // Change category available status
 #region 
-Console.WriteLine("".PadRight(80, '-'));
-Console.WriteLine("Change Category available status");
-var categoryAvailable = await categoryRepository.ChangeCategoryAvailableStatusAsync(3);
-if (categoryAvailable)
-    Console.WriteLine("Change successfully.");
-else
-    Console.WriteLine("Change failed.");
-Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("Change Category available status");
+//var categoryAvailable = await categoryRepository.ChangeCategoryAvailableStatusAsync(3);
+//if (categoryAvailable)
+//    Console.WriteLine("Change successfully.");
+//else
+//    Console.WriteLine("Change failed.");
+//Console.WriteLine("".PadRight(80, '-'));
 #endregion
 
--- Service.Console/Program.cs
+++ Service.Console/Program.cs
@@ -14,50 +14,50 @@ seeder.Initialize();
 
 // U2.2 Create Service
 #region 
-Console.WriteLine("".PadRight(80, '-'));
-Console.WriteLine("Create Service");
+//Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("Create Service");
 
-var createService = new Service()
-{
-    Id = 10,
-    Name = "Dich vu test",
-    ShortDescription = "Dich vu test  ShortDescription",
-    Description = "Dich vu test  Description",
-    CategoryId = 1
-};
-var createdService = await serviceRepository.CreateServiceAsync(createService);
-Console.WriteLine("Id: " + createdService.Id);
-Console.WriteLine("Name: " + createdService.Name);
-Console.WriteLine("Short Description: " + createdService.ShortDescription);
-Console.WriteLine("Description: " + createdService.Description);
-Console.WriteLine("Isdeleted: " + createdService.IsDeleted);
-Console.WriteLine("Available: " + createdService.Available);
-Console.WriteLine("CategoryId: " + createdService.CategoryId);
-Console.WriteLine("".PadRight(80, '-'));
+//var createService = new Service()
+//{
+//    Id = 10,
+//    Name = "Dich vu test",
+//    ShortDescription = "Dich vu test  ShortDescription",
+//    Description = "Dich vu test  Description",
+//    CategoryId = 1
+//};
+//var createdService = await serviceRepository.CreateServiceAsync(createService);
+//Console.WriteLine("Id: " + createdService.Id);
+//Console.WriteLine("Name: " + createdService.Name);
+//Console.WriteLine("Short Description: " + createdService.ShortDescription);
+//Console.WriteLine("Description: " + createdService.Description);
+//Console.WriteLine("Isdeleted: " + createdService.IsDeleted);
+//Console.WriteLine("Available: " + createdService.Available);
+//Console.WriteLine("CategoryId: " + createdService.CategoryId);
+//Console.WriteLine("".PadRight(80, '-'));
 #endregion
 
 // Change Service delete status
 #region
-Console.WriteLine("".PadRight(80, '-'));
-Console.WriteLine("Change Service delete status");
-var deleteStatus = await serviceRepository.ChangeServiceDeleteStatusAsync(2);
-if (deleteStatus)
-    Console.WriteLine("Change successfully.");
-else
-    Console.WriteLine("Change failed.");
-Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("".PadRight(80, '-'));
+//Console.WriteLine("Change Service delete status");
+//var deleteStatus = await serviceRepository.ChangeServiceDeleteStatusAsync(2);
+//if (deleteStatus)
+//    Console.WriteLine("Change successfully.");
+//else
+//    Console.WriteLine("Change failed.");
+//Console.WriteLine("".PadRight(80, '-'));
 #endregion
 
 // Get Services
 #region
-var services = await serviceRepository.GetServicesAsync();
+//var services = await serviceRepository.GetServicesAsync();
 
-Console.WriteLine("{0,-5}{1,-10}{2,40}{3,55}",
-    "ID", "Name", "Short Description" , "Description");
+//Console.WriteLine("{0,-5}{1,-10}{2,40}{3,55}",
+//    "ID", "Name", "Short Description" , "Description");
 
-foreach (var item in services)
-{
-    Console.WriteLine("{0,-5}{1,-10}{2,40}{3,55}",
-        item.Id, item.Name, item.ShortDescription, item.Description);
-}
+//foreach (var item in services)
+//{
+//    Console.WriteLine("{0,-5}{1,-10}{2,40}{3,55}",
+//        item.Id, item.Name, item.ShortDescription, item.Description);
+//}
 #endregion
