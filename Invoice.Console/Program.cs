870d2d18932f0d51b6b544f43cf8d9c23028a17a
dunlok <myloc442@gmail.com>
2023-05-16 14:26:34 +0700

    Add feature/modify the information regarding the room's service usage

--- Invoice.Console/Program.cs
+++ Invoice.Console/Program.cs
@@ -131,3 +131,51 @@ seeder.Initialize();
 //}
 
 #endregion
+
+
+#region Task: Update service usaged in room
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Task: To modify the information regarding the room's service usage");
+
+//int invoiceId = 1;
+
+//var servicesInvoices = new List<ServicesInvoiceDto>()
+//{
+//    new()
+//    {
+//        ServiceId = 8,
+//        Quantity = 2,
+//    },
+//    new()
+//    {
+//        ServiceId = 2,
+//        Quantity = 5,
+//    }
+//};
+
+//var invoice = await repository.UpdateServicesInvoiceAsync(invoiceId, servicesInvoices);
+
+//if (invoice != null)
+//{
+
+//    Console.WriteLine("".PadRight(80, '-'));
+//    Console.WriteLine("Update services invoice");
+
+//    Console.WriteLine("Id: " + invoice.Id);
+//    Console.WriteLine("RoomId: " + invoice.RoomId);
+//    Console.WriteLine("PaymentDate: " + invoice.PaymentDate);
+//    Console.WriteLine("Note: " + invoice.Note);
+//    foreach (var servicesInvoice in invoice.ServicesInvoices)
+//    {
+
+//        Console.WriteLine("ServiceId: " + servicesInvoice.ServiceId);
+//        Console.WriteLine("Price: " + servicesInvoice.Price);
+//        Console.WriteLine("Quantity: " + servicesInvoice.Quantity);
+//    }
+//    Console.WriteLine("".PadRight(80, '-'));
+//}
+//else
+//{
+//    Console.WriteLine("Update failed");
+//}
+#endregion
--- ServiceManagement.Services/ServiceHotel/IInvoiceRepository.cs
+++ ServiceManagement.Services/ServiceHotel/IInvoiceRepository.cs
@@ -11,5 +11,6 @@ public interface IInvoiceRepository
 
 	Task<Invoice> CreateInvoiceAsync(Invoice invoice, CancellationToken cancellationToken = default);
 	Task<Invoice> AddServicesInvoiceAsync(int invoiceId, IList<ServicesInvoiceDto> servicesInvoices, CancellationToken cancellationToken = default);
-	Task<bool> DeletedInvoiceAsync(int invoiceId, CancellationToken cancellationToken = default);
+    Task<Invoice> UpdateServicesInvoiceAsync(int invoiceId, IList<ServicesInvoiceDto> servicesInvoices, CancellationToken cancellationToken = default);
+    Task<bool> DeletedInvoiceAsync(int invoiceId, CancellationToken cancellationToken = default);
 }
